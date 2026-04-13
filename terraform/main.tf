7d976279bfa19b8a95d101684652d848ddb2775c
mysak7 <michal.burdik@proton.me>
2026-04-13 08:24:57 +0200

    fix: change VM size from Standard_B2s to Standard_D2s_v3 for SwedenCentral availability

--- terraform/main.tf
+++ terraform/main.tf
@@ -212,7 +212,7 @@ resource "azurerm_linux_virtual_machine" "main" {
   name                = "${local.project}-vm"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
-  size                = "Standard_B2s"
+  size                = "Standard_D2s_v3"
   admin_username      = "azureuser"
 
   network_interface_ids = [azurerm_network_interface.main.id]
